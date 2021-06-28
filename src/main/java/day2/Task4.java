a454175de48802ca9b0a9f487422ae962d54d8ad
Stas <d1g1texavd@gmail.com>
2021-06-28 22:02:29 +0300

    Day 2 task 4 update 1

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,26 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        double x = scanner.nextDouble();
+        double y = 0;
 
+        if(x >= 5) {
+            y = (Math.pow(x, 2) - 10) / (x + 7);
+        }
+        else if(-3 < x && x < 5) {
+            y = (x+ 3) + (Math.pow(x, 2) - 2);
+        }
+        else {
+            y = 420;
+        }
+
+        String result = String.format("%.10f", y);
+
+        System.out.println(result);
     }
 }
