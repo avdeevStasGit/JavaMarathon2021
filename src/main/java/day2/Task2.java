a05b16de32dd3211d06a6cfdd1d5bd508ab9074e
Stas <d1g1texavd@gmail.com>
2021-06-28 20:26:01 +0300

    Day 2 task 2

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число от: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите число до: ");
+        int b = scanner.nextInt();
 
+        for(int i = a; i < b; i++) {
+
+            boolean x = i % 5 == 0;
+            boolean y = i % 10 != 0;
+
+            if((x) && (y)) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
