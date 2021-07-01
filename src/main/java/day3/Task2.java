8ac332af207032885c858675aea758c70b9d9f91
Stas <d1g1texavd@gmail.com>
2021-07-01 18:50:15 +0300

    Day 3 task 2 update 1

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,40 @@
 package day3;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+/**
+ * 2. Реализовать программу, которая пока работает, принимает на вход от пользователя два числа - делимое и делитель. Для этих двух чисел программа
+ * рассчитывает результат деления и выводит его в консоль. Программа останавливает свою работу тогда, когда пользователь вводит 0 в качестве делителя.
+ * (для этих вещественных чисел необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно). В решении обязательно использовать
+ * оператор break.
+ *
+ * Пример:
+ * 1 2 - ваш ввод в консоль
+ * 0.5 - ответ программы
+ * 100 77 - ваш ввод в консоль
+ * 1.2987012987012987 - ответ программы
+ * 3 0 - ваш ввод в консоль
+ * работа программы завершается
+ */
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Введите делимое:");
+            double dividend = scanner.nextDouble();
+            System.out.println("Введите делитель:");
+            double divider = scanner.nextDouble();
+
+            if(divider == 0) {
+                break;
+            }
+
+            System.out.println(dividend / divider);
+
+        }
 
     }
 }
