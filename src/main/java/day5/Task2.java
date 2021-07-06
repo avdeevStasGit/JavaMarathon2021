e83e32e79042c9e95724efaad3170a729db550cf
Stas <d1g1texavd@gmail.com>
2021-07-06 08:25:27 +0300

    Day 5 task 2 update 1

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,16 @@
 package day5;
 
+/**
+ * 2. Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
+ * Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+ * Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+ * Вывести в консоль значение каждого из полей, используя get методы.
+ */
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2008, "Red", "BMW");
 
+        System.out.println("Описание: " + "год " + motorbike.getAge() + ", цвет " + motorbike.getColor() + ", модель " + motorbike.getModel());
     }
 }
