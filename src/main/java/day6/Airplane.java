c14d4e80880590d0f850c7c38f5e0983dddd5fb1
Stas <d1g1texavd@gmail.com>
2021-07-06 18:24:17 +0300

    Day 6 task 2 update 1

---
+++ src/main/java/day6/Airplane.java
@@ -0,0 +1,41 @@
+package day6;
+
+public class Airplane {
+    private String producer; //(изготовитель)
+    private int year; //(год выпуска)
+    private int length; //(длина)
+    private int weight; //(вес)
+    private int fuel = 0; //(количество топлива в баке)
+
+    public Airplane(String producer, int year, int length, int weight, int fuel) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+}
