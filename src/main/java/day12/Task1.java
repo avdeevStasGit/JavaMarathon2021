f8f403c8fd76672d2bb3191abef432270ca42875
Stas <d1g1texavd@gmail.com>
2021-07-14 11:06:07 +0300

    Day 12 task 1 update 1

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,25 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Lada");
+        list.add("Renault");
+        list.add("Wolkswagen");
+        list.add("Scoda");
+        list.add("Porshe");
 
+        System.out.println(list);
+
+        list.add(2, "Ferrari");
+
+        System.out.println(list);
+
+        list.remove(0);
+
+        System.out.println(list);
     }
 }
