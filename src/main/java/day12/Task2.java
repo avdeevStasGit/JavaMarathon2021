44fa5b50720e24fe816b510b9e002191a116d613
Stas <d1g1texavd@gmail.com>
2021-07-14 12:50:08 +0300

    Day 12 task 2 update 1

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,19 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 31; i++) {
+            if (i % 2 == 0) {
+                list.add(i);
+            }
+        }
+        for(int j = 300; j < 351; j++) {
+            list.add(j);
+        }
+        System.out.println(list);
     }
 }
