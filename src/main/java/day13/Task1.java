d3069627fcb837eab970b99697e61332295cfc73
Stas <d1g1texavd@gmail.com>
2021-08-02 11:57:42 +0300

    Day 13 task 1 update 8 Completed

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -15,6 +15,27 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("Станислав");
+        User user2 = new User("Кирилл");
+        User user3 = new User("Иван");
 
+        user1.sendMessage(user2, "Первое сообщение");
+        user1.sendMessage(user2, "Второе сообщение");
+
+        user2.sendMessage(user1, "Первое сообщение");
+        user2.sendMessage(user1, "Второе сообщение");
+        user2.sendMessage(user1, "Третье сообщение");
+
+        user3.sendMessage(user1, "Первое сообщение");
+        user3.sendMessage(user1, "Второе сообщение");
+        user3.sendMessage(user1, "Третье сообщение");
+
+        user1.sendMessage(user3, "Первое сообщение");
+        user1.sendMessage(user3, "Второе сообщение");
+        user1.sendMessage(user3, "Третье сообщение");
+
+        user3.sendMessage(user1, "Первое сообщение");
+
+        MessageDatabase.showDialog(user1, user3);
     }
 }
