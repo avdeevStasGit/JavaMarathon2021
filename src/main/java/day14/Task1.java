248e8e6533407735a339761a91204eef18f132ee
Stas <d1g1texavd@gmail.com>
2021-08-03 12:43:56 +0300

    Day 14 task 1 update 1

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,13 @@
 package day14;
 
+import java.io.File;
+
 public class Task1 {
     public static void main(String[] args) {
+
+    }
+
+    public static void printSumDigits(File file) {
 
     }
 }
