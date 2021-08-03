d3bb3668584bfc1d8693af17c6633bb0e4fa2efb
Stas <d1g1texavd@gmail.com>
2021-08-03 13:40:19 +0300

    Day 17 task 1 update 3

--- src/main/java/day17/ChessPiece.java
+++ src/main/java/day17/ChessPiece.java
@@ -6,7 +6,8 @@ public enum ChessPiece {
     ROOK_WHITE(5, "♖"), ROOK_BLACK(5,"♜"),
     BISHOP_WHITE(3.5, "♗"), BISHOP_BLACK(3.5, "♝"),
     KNIGHT_WHITE(3, "♘"), KNIGHT_BLACK(3, "♞"),
-    PAWN_WHITE(1, "♙"), PAWN_BLACK(1, "♟");
+    PAWN_WHITE(1, "♙"), PAWN_BLACK(1, "♟"),
+    EMPTY(-1, "_");
 
     private double value;
     private String represitation;
