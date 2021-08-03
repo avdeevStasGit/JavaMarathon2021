77881c6112e3906aeaf5dd8fad22ef9481215886
Stas <d1g1texavd@gmail.com>
2021-08-03 13:38:59 +0300

    Day 17 task 1 update 2

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,13 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] pieaces = {
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+            ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK
+        };
 
+        for( ChessPiece piece : pieaces) {
+            System.out.print(piece.getRepresitation() + " ");
+        }
     }
 }
